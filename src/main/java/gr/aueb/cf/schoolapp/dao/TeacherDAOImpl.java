2127c1f1bf17d60205af576dc4cfc0f5d4baafb2
Kanellos Almpanis <kanellosalmpanis@hotmail.com>
2024-07-24 21:21:03 +0300

    updates

--- src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
@@ -2,32 +2,82 @@ package gr.aueb.cf.schoolapp.dao;
 
 import gr.aueb.cf.schoolapp.dao.exceptions.TeacherDAOException;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.service.util.DBUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
-public interface TeacherDAOImpl implements ITeacherDAO {
+public class TeacherDAOImpl implements ITeacherDAO {
+
+
     @Override
-    default Teacher insert(Teacher teacher) throws TeacherDAOException {
+    public Teacher insert(Teacher teacher) throws TeacherDAOException {
+        String sql = "INSERT INTO teachers (firstname, lastname) VALUES (?, ?)";
+
+        try  (Connection connection = DBUtil.getConnection();
+              PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            // extract model info
+            String firstname = teacher.getFirstname();
+            String lastname = teacher.getLastname();
+
+            // set
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+
+            int n = ps.executeUpdate();     // returns how many rows were affected
+            // logging
+            return teacher;                 // TBD
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in inserting teacher: " + teacher);
+        }
+    }
+
+    @Override
+    public Teacher update(Teacher teacher) throws TeacherDAOException {
+        String sql = "UPDATE teachers SET firstname = ?, lastname = ? WHERE id = ?";
+
+        try  (Connection connection = DBUtil.getConnection();
+              PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            // extract model info
+            int id = teacher.getId();
+            String firstname = teacher.getFirstname();
+            String lastname = teacher.getLastname();
+
+            // set
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+            ps.setInt(3, id);
+
+            int n = ps.executeUpdate();     // returns how many rows were affected
+            // logging
+            return teacher;                 // TBD
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in inserting teacher: " + teacher);
+        }
+    }
+
+    @Override
+    public void delete(Integer id) throws TeacherDAOException {
+
+    }
+
+    @Override
+    public Teacher getById(Integer id) throws TeacherDAOException {
         return null;
     }
 
     @Override
-    default Teacher update(Teacher teacher) throws TeacherDAOException {
-        return null;
-    }
-
-    @Override
-    default void delete(Integer id) throws TeacherDAOException {
-
-    }
-
-    @Override
-    default Teacher getById(Integer id) throws TeacherDAOException {
-        return null;
-    }
-
-    @Override
-    default List<Teacher> getByLastName(String lastname) throws TeacherDAOException {
+    public List<Teacher> getByLastName(String lastname) throws TeacherDAOException {
         return List.of();
     }
 }
